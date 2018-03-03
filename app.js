8e9eee90d630dcda3aadf84b569173306a38000e
frankc60 <frankc60@gmail.com>
2018-03-03 19:24:18 +0000

    added basic router

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-//
+//app.js
+
+const express = require("express");
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+//console.log(process.env.PORT);
+//in linux, from bash, export PORT=8080
+
+const routes = require('./routes'); //routes.js
+
+app.use(morgan('combined'));
+
+// load the cookie-parsing middleware
+app.use(cookieParser());
+
+// load the body-parsing middleware
+app.use(bodyParser());
+
+app.use(routes);
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`))
+
+
---
+++ routes.js
@@ -0,0 +1,28 @@
+//routes.js
+const express = require("express");
+const app = express();
+
+    //run for ALL requests
+    app.use(function (req, res, next) {
+        //console.log(`Timestamp: ${new Date(Date.now())}`);
+        //morgan is displaying connection info!
+        next();
+    });
+
+    app.get('/', (req, res) => {
+        res.send('GET Hello World!');
+    });
+
+    app.post('/', (req, res) => {
+        res.send('POST Hello World!');
+    });
+
+    app.delete('/', (req, res) => {
+        res.send('DELETE Hello World!');
+    });
+
+    app.put('/', (req, res) => {
+        res.send('PUT Hello World!');
+    });
+
+module.exports = app;
