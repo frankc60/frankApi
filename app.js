c8edf8504c06208ee5f8d0185baa75114fb70162
frankc60 <frankc60@gmail.com>
2018-03-04 20:56:32 +0000

    test, nothing

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ const routes = require('./routes'); //routes.js
 const app = express();
 
 //--------------------------------------------------------------------------------------------
-
-// load the cookie-parsing middleware
-//app.use(cookieParser());
+//use middleware
 app.use(morgan);
 app.use(routes);
 
@@ -20,6 +18,5 @@ app.use(routes);
 const port = process.env.PORT || 3000;
 //console.log(process.env.PORT);
 //in linux, from bash, >export PORT=8080
-
 
 app.listen(port, () => console.log(`frankApi listening on port ${port}`));
