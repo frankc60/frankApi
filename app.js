549e0f7be2e639774cbc66ed03b9e4acb9c287aa
frankc60 <frankc60@gmail.com>
2018-03-11 20:43:04 +0000

    added error handling and prettier to files

--- app.js
+++ app.js
@@ -21,22 +21,19 @@ app.disable("x-powered-by"); //security
 
 app.use(compression());
 app.use(morgan);
-
 //app.use("/static", express.static('public'));
-
 app.use(favicon(__dirname + "/public/images/favicon.ico"));
 
 app.use("/", routes);
-//if doesn't find route from above then if it returns and continues, it is a 404, handle below:
-app.use(function(req, res, next) {
-    //if we get here, then no other routes have got the route, so is a 404.
-    res.status(404);
-    res.send("error 404");
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send(`error 500: ${err}`);
 });
 
-//console.log(process.env.PORT);
-//in linux, from bash, >export PORT=8080
-
-//app.listen(port, () => console.log(`frankApi listening on port ${port}`));
+//if doesn't find route from above then if it returns and continues, it is a 404, handle below:
+app.use(function(req, res, next) {
+    res.status(404).send("error 404");
+});
 
 module.exports = app;
